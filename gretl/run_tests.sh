2887222c89f686a62e28c4aa5246617db810b213
Artur Tarassow <artur.tarassow@otto.de>
2021-06-15 09:24:37 +0200

    print name of script tested

--- gretl/run_tests.sh
+++ gretl/run_tests.sh
@@ -14,6 +14,7 @@ cd $DIR
 echo "Switched to ${DIR}"
 
 
+echo "Start with script '${SCRIPT1}'."
 gretlcli -b -e -q ${SCRIPT1}
 if [ $? -eq 0 ]
 then
@@ -22,7 +23,9 @@ else
   echo "Failure: Tests for script ${SCRIPT1} not passed."
   exit 1
 fi
+echo "Finished script '${SCRIPT1}'."
 
+echo "Start with script '${SCRIPT2}'."
 gretlcli -b -e -q ${SCRIPT2}
 if [ $? -eq 0 ]
 then
@@ -31,8 +34,10 @@ else
   echo "Failure: Tests for script ${SCRIPT2} not passed."
   exit 1
 fi
+echo "Finished script '${SCRIPT2}'."
 
 
+echo "Start with script '${SCRIPT3}'."
 gretlcli -b -e -q ${SCRIPT3}
 if [ $? -eq 0 ]
 then
@@ -41,8 +46,10 @@ else
   echo "Failure: Tests for script ${SCRIPT3} not passed."
   exit 1
 fi
+echo "Finished script '${SCRIPT3}'."
 
 
+echo "Start with script '${SCRIPT4}'."
 gretlcli -b -e -q ${SCRIPT4}
 if [ $? -eq 0 ]
 then
@@ -51,6 +58,7 @@ else
   echo "Failure: Tests for script ${SCRIPT4} not passed." >&2
   exit 1
 fi
+echo "Finished script '${SCRIPT4}'."
 
 
 exit 0
